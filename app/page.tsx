053234ec318168fe8c30e5baa125204b7692decb
vikas lh <146969780+Vikaslh@users.noreply.github.com>
2025-07-20 19:53:10 +0530

    just start clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,52 @@
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
-  return (<div>
-    <h1 className="text-2xl underline">Welcome to saas app</h1>
-    <Button>
-      les's get sstarted
-    </Button>
-    </div>
+  return (
+  <main>
+    <h1 className="text-2xl underline">Popular Companions</h1>
+    <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the Brainy Explorer"
+                topic="Neural Network of the Brain"
+                subject="science"
+                duration={45}
+                color="#ffda6e" bookmarked={false}
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Derivatives & Integrals"
+                subject="maths"
+                bookmarked={false}
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF" bookmarked={false}            />
+        </section>
+
+   <section className="home-section">
+   <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+    <CTA/>
+
+    </section>
+
+  </main>
   )
 }
 
